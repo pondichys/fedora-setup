7c50dedd4f4981e3571a7acf420be384bc89b74a
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-12-09 10:10:12 +0100

    feat(base): add lsd as ls replacement

--- 00-base.sh
+++ 00-base.sh
@@ -2,7 +2,7 @@ echo "Installing Z shell"
 sudo dnf install -y zsh zsh-autosuggestions zsh-syntax-highlighting
 
 echo "Installing base software"
-sudo dnf install -y bat curl fd-find fzf gh git-delta jq meld ripgrep stow tealdeer tmux tree zoxide
+sudo dnf install -y bat curl fd-find fzf gh git-delta jq lsd meld ripgrep stow tealdeer tmux tree zoxide
 
 echo "Install kitty terminal"
 sudo dnf install -y kitty
@@ -10,3 +10,5 @@ sudo dnf install -y kitty
 echo "Install distrobox"
 sudo dnf install -y distrobox
 
+echo "Install podman complementary packages"
+sudo dnf install -y podman-compose podman-docker
