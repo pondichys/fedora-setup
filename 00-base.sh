53e39201d187293ca0aef452b7cc2260d9bdb37f
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-12-30 16:15:49 +0100

    feat: configure dnf (part 1)

--- 00-base.sh
+++ 00-base.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/env bash
+echo "DNF configuration ..."
+if [ ! $(grep -ic 'fastestmirror=' /etc/dnf/dnf.conf) ]; then
+    echo -n "Use fastest DNF mirrors"
+    echo "fastestmirror=True" >> /etc/dnf/dnf.conf
+fi
+
+if [ ! $(grep -ic 'max_parallel_dowwnloads=' /etc/dnf/dnf.conf) ]; then
+    echo -n "Use 10 parallel download streams"
+    echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+fi
+
 echo "Installing Z shell"
 sudo dnf install -y zsh zsh-autosuggestions zsh-syntax-highlighting
 
