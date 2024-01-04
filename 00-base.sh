3cc0bdd8e699738a07a272eddffc3d2c93718004
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-01-04 10:09:46 +0100

    feat: add ms vscode installation

--- 00-base.sh
+++ 00-base.sh
@@ -48,6 +48,12 @@ sudo dnf install -y podman-compose podman-docker
 echo "Install gnome-shell extensions"
 sudo dnf install -y gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell
 
+echo "Install VSCode from Microsoft"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
+
 # FLATPAKS
 echo "Configure Flathub if needed ..."
 if [ ! $(flatpak remotes | grep -c flathub) ]; then
