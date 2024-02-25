f624a6418a2d508bf571bea3e2c85ed3457d6aa4
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-02-25 12:29:09 +0100

    fix: make homebrew installation more robust

--- brew.sh
+++ brew.sh
@@ -1,22 +1,28 @@
+#! /usr/bin/env bash
+
 echo "Install HomeBrew dependencies"
 sudo dnf groupinstall -y 'Development Tools'
 
-echo "Install HomeBrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install HomeBrew if not already present on the system
+if test ! -d /home/linuxbrew
+then
+	echo "Install HomeBrew"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.bash_profile
+# Configure this instance of bash shell to install homebrew packages
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 echo "Install software available through HomeBrew"
-brew install azure-cli
+# brew install azure-cli
 brew install bitwarden-cli
-brew install chezmoi
-brew install fnm
+# brew install chezmoi
+# brew install fnm
 brew install helm
 brew install k9s
 brew install kubie
-brew install lazygit
+# brew install lazygit
 brew install scw
-brew install starship
+# brew install starship
 brew install yazi
 brew install yq
