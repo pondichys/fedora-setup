9fd994654580be4ce7f20ade4df30fc5e57ec0ff
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-02-25 12:11:52 +0100

    feat(homebrew): add some packages to install

--- brew.sh
+++ brew.sh
@@ -8,10 +8,15 @@ echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.bash_profil
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 echo "Install software available through HomeBrew"
-brew install bottom
+brew install azure-cli
+brew install bitwarden-cli
+brew install chezmoi
+brew install fnm
+brew install helm
+brew install k9s
+brew install kubie
 brew install lazygit
 brew install scw
 brew install starship
-
-
-
+brew install yazi
+brew install yq
